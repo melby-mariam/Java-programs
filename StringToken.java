e735d5703a36a7a08951cfc33bc51cc5131bfe34
melby-mariam <105125128+melby-mariam@users.noreply.github.com>
2023-07-05 14:02:15 +0530

    Add files via upload

---
+++ StringToken.java
@@ -0,0 +1,26 @@
+package lab22;
+
+import java.util.*;
+public class StringToken {
+
+	public static void main(String[] args) {
+		
+		int n;
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter integers with one space gap:");
+		String s = sc.nextLine();
+		StringTokenizer st = new StringTokenizer(s, " ");
+		while (st.hasMoreTokens())
+		{
+		String temp = st.nextToken();
+		n = Integer.parseInt(temp);
+		System.out.println(temp);
+		sum = sum+n;
+		}
+		System.out.println("sum of the integers is: " + sum);
+		sc.close();
+		}
+		}
+
+			
